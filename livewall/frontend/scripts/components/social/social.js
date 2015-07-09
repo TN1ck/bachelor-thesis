610ed56fcd7671a4c191d1635de8cb4cb7f591f3
TN1ck <tomwanick@gmail.com>
2015-07-09 09:36:02 +0200

    cleaned up stats site

--- livewall/frontend/scripts/components/social/social.js
+++ livewall/frontend/scripts/components/social/social.js
@@ -1,29 +1,22 @@
 import React       from 'react';
 import Reflux      from 'reflux';
-var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
-import {
-    Grid, Row, Col, Input, Button,
-    Jumbotron, Alert, PageHeader
-}                  from 'react-bootstrap';
+import {Row, Col}  from 'react-bootstrap';
 
+import t           from '../../../shared/translations/translation.js';
 import gameStore   from '../../stores/game.js';
 import BarChart    from '../charts/barChart.js';
 import LeaderBoard from '../badges/leaderboard.js';
+import Action      from './action.js';
 
-import t from '../../../shared/translations/translation.js';
-import {colors} from '../../../shared/util/colors.js';
-
-import Action from './action.js';
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default React.createClass({
     mixins: [
         Reflux.connect(gameStore),
     ],
-    render: function () {
+    createBarChart: function () {
 
         var alltime = this.state.alltime;
-        var monthly = this.state.monthly;
-        var actions = this.state.actions;
 
         var data = [
             {
@@ -53,6 +46,39 @@ export default React.createClass({
 
         ].sort((a, b) => b.x - a.x);
 
+        return (
+            <Col xs={12} sm={12} md={3}>
+                <h1>{alltime.points} Punkte</h1>
+                <hr/>
+                <BarChart data={data}/>
+            </Col>
+        );
+
+    },
+    createLeaderBoards: function () {
+
+        var alltime = this.state.alltime;
+        var monthly = this.state.monthly;
+
+        return (
+            <Col xs={12} sm={12} md={6}>
+                <h1>{t.badgesPage.leaderboard.header}</h1>
+                <hr/>
+                <Row>
+                    <Col md={12} lg={6}>
+                        <h3>{t.badgesPage.leaderboard.alltime}</h3>
+                        <LeaderBoard users={alltime.users}/>
+                    </Col>
+                    <Col md={12} lg={6}>
+                        <h3>{t.badgesPage.leaderboard.monthly}</h3>
+                        <LeaderBoard users={monthly.users}/>
+                    </Col>
+                </Row>
+            </Col>
+        );
+    },
+    createActions: function () {
+        var actions = this.state.actions;
         var _actions = actions.map(action => {
             return (
                 <Row key={action.action.id}>
@@ -62,33 +88,21 @@ export default React.createClass({
         });
 
         return (
+            <Col xs={12} sm={12} md={3}>
+                <h1>Aktionen</h1>
+                <hr/>
+                <ReactCSSTransitionGroup transitionName="fade" transitionAppear={true} transitionEnter={true}>
+                    {_actions}
+                </ReactCSSTransitionGroup>
+            </Col>
+        );
+    },
+    render: function () {
+        return (
             <Row>
-                <Col xs={12} sm={12} md={6}>
-                    <h1>{t.badgesPage.leaderboard.header}</h1>
-                    <hr/>
-                    <Row>
-                        <Col md={12} lg={6}>
-                            <h3>{t.badgesPage.leaderboard.alltime}</h3>
-                            <LeaderBoard users={alltime.users}/>
-                        </Col>
-                        <Col md={12} lg={6}>
-                            <h3>{t.badgesPage.leaderboard.monthly}</h3>
-                            <LeaderBoard users={monthly.users}/>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col xs={12} sm={12} md={3}>
-                    <h1>{alltime.points} Punkte</h1>
-                    <hr/>
-                    <BarChart data={data}/>
-                </Col>
-                <Col xs={12} sm={12} md={3}>
-                    <h1>Aktionen</h1>
-                    <hr/>
-                    <ReactCSSTransitionGroup transitionName="fade" transitionAppear={true} transitionEnter={true}>
-                        {_actions}
-                    </ReactCSSTransitionGroup>
-                </Col>
+                {this.createLeaderBoards()}
+                {this.createBarChart()}
+                {this.createActions()}
             </Row>
         );
     }
