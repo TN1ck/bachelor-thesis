341c1557f49b6a536d5a666f2cdb0af894bc9a79
TN1ck <tomwanick@gmail.com>
2017-09-10 20:31:33 +0200

    also install dev deps

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@ CURRENT_DIR=`pwd`
 
 # install packages
 
-cd $CURRENT_DIR/infoboard/frontend && npm install
+cd $CURRENT_DIR/infoboard/frontend && npm install && npm install --only=dev
 cd $CURRENT_DIR/infoboard/backend && npm install
 
 # build frontend
 cd $CURRENT_DIR/infoboard/frontend
-node webpack-build
-rm -rf dist
-mkdir dist && cp index.html ./dist && mv build ./dist/build
+node webpack-build && sleep 10 && rm -rf dist && mkdir dist && cp index.html ./dist && mv build ./dist/build
